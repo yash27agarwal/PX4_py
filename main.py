42dc3c13a67dff3ec7a74eb43a52cf6cf741d76f
Yash Agarwal <yash.27.agarwal@gmail.com>
2024-10-11 11:00:57 +0800

    fixed variables name and their values

--- main.py
+++ main.py
@@ -16,23 +16,23 @@ class InnerLoopControlImpl:
         self.current_angular_rate = [0, 0, 0]
 
         # Initialize controllers using YAML values
-        self.controller = AttitudeController(KP_ROLL_ANGLE=attitude_config['KP_ROLL_ANGLE'], 
-                                             KP_PITCH_ANGLE=attitude_config['KP_PITCH_ANGLE'], 
-                                             KP_YAW_ANGLE=attitude_config['KP_YAW_ANGLE'], 
+        self.controller = AttitudeController(KP_ROLL_ANGLE=attitude_config['MC_ROLL_P'], 
+                                             KP_PITCH_ANGLE=attitude_config['MC_PITCH_P'], 
+                                             KP_YAW_ANGLE=attitude_config['MC_YAW_P'], 
                                              MC_YAW_WEIGHT=attitude_config['MC_YAW_WEIGHT'], 
                                              MAX_CONTROL_ANGLE_RATE_PITCH=attitude_config['MAX_CONTROL_ANGLE_RATE_PITCH'], 
                                              MAX_CONTROL_ANGLE_RATE_ROLL=attitude_config['MAX_CONTROL_ANGLE_RATE_ROLL'], 
                                              MAX_CONTROL_ANGLE_RATE_YAW=attitude_config['MAX_CONTROL_ANGLE_RATE_YAW'])
         
-        self.angular_rate_controller = AngularRateController(KP_ROLL_RATE=angular_rate_config['KP_ROLL_RATE'], 
-                                                             KI_ROLL_RATE=angular_rate_config['KI_ROLL_RATE'], 
-                                                             KD_ROLL_RATE=angular_rate_config['KD_ROLL_RATE'],
-                                                             KP_PITCH_RATE=angular_rate_config['KP_PITCH_RATE'], 
-                                                             KI_PITCH_RATE=angular_rate_config['KI_PITCH_RATE'], 
-                                                             KD_PITCH_RATE=angular_rate_config['KD_PITCH_RATE'],
-                                                             KP_YAW_RATE=angular_rate_config['KP_YAW_RATE'], 
-                                                             KI_YAW_RATE=angular_rate_config['KI_YAW_RATE'], 
-                                                             KD_YAW_RATE=angular_rate_config['KD_YAW_RATE'], 
+        self.angular_rate_controller = AngularRateController(KP_ROLL_RATE=angular_rate_config['MC_ROLLRATE_P'], 
+                                                             KI_ROLL_RATE=angular_rate_config['MC_ROLLRATE_I'], 
+                                                             KD_ROLL_RATE=angular_rate_config['MC_ROLLRATE_D'],
+                                                             KP_PITCH_RATE=angular_rate_config['MC_PITCHRATE_P'], 
+                                                             KI_PITCH_RATE=angular_rate_config['MC_PITCHRATE_I'], 
+                                                             KD_PITCH_RATE=angular_rate_config['MC_PITCHRATE_D'],
+                                                             KP_YAW_RATE=angular_rate_config['MC_YAWRATE_P'], 
+                                                             KI_YAW_RATE=angular_rate_config['MC_YAWRATE_I'], 
+                                                             KD_YAW_RATE=angular_rate_config['MC_YAWRATE_D'], 
                                                              dt=angular_rate_config['dt'])
         self.mixer = MotorMixer()
         
@@ -43,17 +43,17 @@ class InnerLoopControlImpl:
         yawrate_d = 0.1
 
         # Call attitude control method
-        rate_setpoint, qd, qd2 = self.controller.attitude_control(att_q, att_setpoint_q, yawrate_d)
+        rate_setpoint, _, _ = self.controller.attitude_control(att_q, att_setpoint_q, yawrate_d)
 
         # Call angular rate control method
-        roll, pitch, yaw = self.angular_rate_controller.control(rate_setpoint, self.current_angular_rate)
+        torque_roll, torque_pitch, torque_yaw = self.angular_rate_controller.control(rate_setpoint, self.current_angular_rate)
 
         thrust = 1
 
         # Mix control outputs to motor commands
-        print(self.mixer.mix(torque_roll=roll, 
-                             torque_pitch=pitch, 
-                             torque_yaw=yaw, 
+        print(self.mixer.mix(torque_roll=torque_roll, 
+                             torque_pitch=torque_pitch, 
+                             torque_yaw=torque_yaw, 
                              thrust=thrust)) 
         
 if __name__ == "__main__":
